232d54a10a01a08575993bec8d53e7c4e5e7e770
Kolapo Abolarinwa <kola@Mac.fritz.box>
2025-09-11 21:53:24 +0100

    Add an example of DI

--- springBootStore/store/src/main/java/com/springbootstore/store/StoreApplication.java
+++ springBootStore/store/src/main/java/com/springbootstore/store/StoreApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+//        SpringApplication.run(StoreApplication.class, args);
+
+        var applicationContext = SpringApplication.run(StoreApplication.class);
+        var notificationSender = applicationContext.getBean( NotificationSender.class);
+        notificationSender.sendNotification("Hello World!");
     }
 
 }
