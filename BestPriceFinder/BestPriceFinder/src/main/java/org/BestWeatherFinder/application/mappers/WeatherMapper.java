c6dae8374679e80946e29d634dd34cba189138d3
Kola <kola.abolarinwa@gmail.com>
2026-07-05 01:55:34 +0100

    Refactor some dto and service models classes to records

--- BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/application/mappers/WeatherMapper.java
+++ BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/application/mappers/WeatherMapper.java
@@ -3,15 +3,11 @@ package org.BestWeatherFinder.application.mappers;
 import org.BestWeatherFinder.application.dtos.GetWeatherForecastDto;
 import org.BestWeatherFinder.domain.contracts.models.WeatherForecast;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface WeatherMapper {
     WeatherMapper INSTANCE = Mappers.getMapper(WeatherMapper.class);
 
-    @Mapping(target = "city", expression = "java(String.valueOf(weatherForecast.getCity()))")
-    @Mapping(target = "forecast", expression = "java(String.valueOf(weatherForecast.getForecast()))")
-    @Mapping(target = "temperatureUnit", expression = "java(String.valueOf(weatherForecast.getTemperatureUnit()))")
     GetWeatherForecastDto toDto(WeatherForecast weatherForecast);
 }
--- BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/domain/contracts/models/WeatherForecast.java
+++ BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/domain/contracts/models/WeatherForecast.java
@@ -1,18 +1,14 @@
 package org.BestWeatherFinder.domain.contracts.models;
 
 import lombok.Builder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.BestWeatherFinder.infrastructure.services.weather.enums.City;
 import org.BestWeatherFinder.infrastructure.services.weather.enums.Forecast;
 import org.BestWeatherFinder.infrastructure.services.weather.enums.TemperatureUnit;
 
-@AllArgsConstructor
-@Getter
 @Builder
-public class WeatherForecast {
-    private City city;
-    private Forecast forecast;
-    private double temperature;
-    private TemperatureUnit temperatureUnit;
-}
+public record WeatherForecast(
+    City city,
+    Forecast forecast,
+    double temperature,
+    TemperatureUnit temperatureUnit
+) {}
