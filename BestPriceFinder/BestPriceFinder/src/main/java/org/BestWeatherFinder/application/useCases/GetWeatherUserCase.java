7c853c0d5cd3d1350584f981420f915fe6accdb9
Kola <kola.abolarinwa@gmail.com>
2026-07-05 02:04:19 +0100

    Refactor how weather service is used in GetWeatherForecastDto

--- BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/application/useCases/GetWeatherUserCase.java
+++ BestPriceFinder/BestPriceFinder/src/main/java/org/BestWeatherFinder/application/useCases/GetWeatherUserCase.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.BestWeatherFinder.application.dtos.GetWeatherForecastDto;
 import org.BestWeatherFinder.application.mappers.WeatherMapper;
 import org.BestWeatherFinder.domain.contracts.WeatherService;
-import org.BestWeatherFinder.infrastructure.services.weather.enums.City;
 
 @RequiredArgsConstructor
 public class GetWeatherUserCase {
     private final WeatherService weatherService;
 
     public GetWeatherForecastDto execute(String city) {
-        var weatherForecast = weatherService.getWeatherForecastAsync(city).join();
-        return WeatherMapper.INSTANCE.toDto(weatherForecast);
+       return weatherService.getWeatherForecastAsync(city).thenApplyAsync(WeatherMapper.INSTANCE::toDto).join();
     }
 }
